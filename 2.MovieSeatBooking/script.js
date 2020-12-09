09614470eb758e39f805dbd9a4aa502ccbcd9065
ajofkn <ajofknt@gmail.com>
2020-12-09 15:35:39 -0600

    Added local storage functionality

--- 2.MovieSeatBooking/script.js
+++ 2.MovieSeatBooking/script.js
@@ -1,10 +1,10 @@
 const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
-
 const count = document.getElementById('count');
 const total = document.getElementById('total');
-
 const movieSelect = document.getElementById("movie");
+
+populateUI();
 
 // The + in the front converts t he value into an interger. Other options include parseInt() or Math.floor
 let ticketPrice = +movieSelect.value;
@@ -12,7 +12,7 @@ let ticketPrice = +movieSelect.value;
 // Save selected movie index and price
 function setMovieData(movieIndex, moviePrice) {
     localStorage.setItem('selectedMovieIndex', movieIndex);
-    localStorage.setItem('selectedMoviePrice', price);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
 }
 
 
@@ -31,6 +31,24 @@ function updateSelectedCount(){
     count.innerText = selectedSeatsCount;
 }
 
+// Grab the data from the local storage, and then update the UI to match the information from the local storage
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if(selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach((seat, index)=>{
+            if(selectedSeats.indexOf(index)> -1){
+                seat.classList.add('selected')
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+
+    if(selectedMovieIndex !== null){
+        movieSelect.selectedIndex = selectedMovieIndex
+    }
+}
 
 
 // Puts an event listener on the whole container, and through that, it checks if a seat has been clicked and if so, toggles it
@@ -48,3 +66,6 @@ movieSelect.addEventListener('change', e =>{
     setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
+
+// Initial count and total set
+updateSelectedCount();
